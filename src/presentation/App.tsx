c9e85a64fa991fc8ee04171f19574f8966093382
brayan <brayanjaraba2006@gmail.com>
2026-06-30 11:39:34 -0500

    format

--- src/presentation/App.tsx
+++ src/presentation/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router";
 import "./App.css";
 import { FilterProductsScreen } from "./products/filter/FilterProductsScreeen";
 import { NavigationBar } from "./shared/components/NavigationBar";
+import { CreateNewProductScreen } from "./products/create/CreateNewProductScreen";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<DashBoard />}></Route>
         <Route path="/products" element={<FilterProductsScreen />} />
+        <Route path="/products/create" element={<CreateNewProductScreen />} />
       </Routes>
       <NavigationBar />
     </>
--- src/presentation/shared/components/TopBar.tsx
+++ src/presentation/shared/components/TopBar.tsx
@@ -1,12 +1,25 @@
+import { useNavigate } from "react-router";
+import { IconButton } from "./IconButton";
+
 export type TopBarParams = {
   title: string;
+  navigateBack: boolean;
 };
 
-export function TopBar(params: TopBarParams) {
-  const { title } = params;
+export function TopBar({ title, navigateBack = false }: TopBarParams) {
+  const navigate = useNavigate();
   return (
-    <header className="bg-slate-700 text-blue-50 py-4 max-h-fit">
-      <p className="font-bold text-xl px-4">{title}</p>
+    <header className="bg-slate-700 text-blue-50 py-4 max-h-fit flex flex-1 gap-2 px-4">
+      {navigateBack && (
+        <IconButton
+          icon="sprite.svg#navigate-back"
+          onClick={() => {
+            navigate(-1);
+          }}
+          size="sm"
+        />
+      )}
+      <p className="font-bold text-xl">{title}</p>
     </header>
   );
 }
