53d732eece382cc515528d18e1f5de1043a56d19
brayan <brayanjaraba2006@gmail.com>
2026-07-16 16:34:15 -0500

    feat: format and remove action resize animation

--- src/app/presentation/shared/components/AlertDialog.tsx
+++ src/app/presentation/shared/components/AlertDialog.tsx
@@ -26,24 +26,30 @@ export function AlertDialog({
     } else {
       dialog.removeAttribute("open");
     }
-
   }, [isOpen]);
 
-  return <dialog data-testid="dialog"
-    onClose={onDismiss}
-    ref={dialogRef}>
-    <span data-testid='title'>{title}</span>
-    {
-      description &&
-      <span data-testid='description'>{description}</span>
-    }
+  return (
+    <dialog data-testid="dialog" onClose={onDismiss} ref={dialogRef}>
+      <div className="grid grid-cols-2 gap-4 p-8 bg-gray-800 text-gray-400">
+        <svg className="col-span-2 self-center justify-self-center size-16">
+          <use href='/sprite.svg#alert' />
+        </svg>
+        <span data-testid="title" className="col-span-2 text-2xl font-bold text-center text-white">
+          {title}
+        </span>
+        {description && (
+          <span data-testid="description" className="col-span-2 text-center">
+            {description}
+          </span>
+        )}
 
-    <button onClick={onDismiss} data-testid='dismiss-button'>
-      Cerrar
-    </button>
-    <button onClick={onConfirm} data-testid='confirm-button'>
-      Continuar
-    </button>
-
-  </dialog>;
+        <button className="bg-blue-800 text-white py-1 rounded-xl" onClick={onDismiss} data-testid="dismiss-button">
+          Cerrar
+        </button>
+        <button className="bg-red-800 text-white py-1 rounded-xl" onClick={onConfirm} data-testid="confirm-button">
+          Continuar
+        </button>
+      </div>
+    </dialog>
+  );
 }
