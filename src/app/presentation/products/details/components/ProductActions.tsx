a4f07ce99cb830508352bd915b38f847b3b07b84
brayan <brayanjaraba2006@gmail.com>
2026-07-17 17:21:44 -0500

    fix: element positioning from flex to grid

--- src/app/presentation/products/details/components/ProductActions.tsx
+++ src/app/presentation/products/details/components/ProductActions.tsx
@@ -2,7 +2,7 @@ import { DeleteAction } from "./DeleteAction";
 
 export function ProductActions({ productId }: { productId: string }) {
   return (
-    <div className="flex justify-around">
+    <div className="grid grid-cols-2 gap-4 justify-between px-4">
       <button
         className="bg-blue-800 px-4 py-2 rounded-xl"
         data-testid="edit-button"
