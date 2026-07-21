4a4b9c6f2d4bad04d8265ea25b1af805a6db1b06
brayan <brayanjaraba2006@gmail.com>
2026-07-21 14:54:45 -0500

    fix: typo error

--- src/app/application/products/FilterProductsUseCase.ts
+++ src/app/application/products/FilterProductsUseCase.ts
@@ -2,7 +2,7 @@ import type { Page } from "../../domain/model/Page";
 import type { Product } from "../../domain/model/Product";
 import type { ProductRepositoryPort } from "../../domain/ProductRepositoryPort";
 
-export type FilterProductsCommnad = {
+export type FilterProductsCommand = {
   search: string | null;
   category: string | null;
   page: number | null;
@@ -10,8 +10,8 @@ export type FilterProductsCommnad = {
 
 export function FilterProductsUseCase(
   repository: ProductRepositoryPort,
-): (params: FilterProductsCommnad) => Promise<Page<Product>> {
-  return async (params: FilterProductsCommnad): Promise<Page<Product>> => {
+): (params: FilterProductsCommand) => Promise<Page<Product>> {
+  return async (params: FilterProductsCommand): Promise<Page<Product>> => {
     const { search, category, page } = params;
     return repository.findAll(search, category, page);
   };
