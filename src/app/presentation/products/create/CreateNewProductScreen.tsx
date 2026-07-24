110cbd4754a5b446f7fe01dbc071578cb3bec8c7
brayan <brayanjaraba2006@gmail.com>
2026-07-24 18:00:53 -0500

    feat: add navigation back to component

--- src/app/presentation/products/create/CreateNewProductScreen.tsx
+++ src/app/presentation/products/create/CreateNewProductScreen.tsx
@@ -1,12 +1,17 @@
+import { useNavigate } from "react-router";
 import { TopBar } from "../../shared/components/TopBar.tsx";
 import { CreateNewProductForm } from "./components/CreateNewProductForm.tsx";
 
 export function CreateNewProductScreen() {
+  const navigate = useNavigate();
+  const navigateBack = () => {
+    navigate(-1);
+  };
   return (
     <main className="flex flex-col flex-1 gap-2 bg-gray-900 text-gray-100  min-h-0">
       <TopBar title="Agregar producto nuevo" navigateBack />
       <section className="px-4">
-        <CreateNewProductForm />
+        <CreateNewProductForm navigateBack={navigateBack} />
       </section>
     </main>
   );
